eca5ce9b0eef7dd2aac88dc0336c05f69f0bd5df
HyunWoo <shw7349@naver.com>
2022-11-21 23:27:36 +0900

    message:
    q!
    https://github.com/shw7349/1day1git.git
    cccc
    .,
    ;;
    '
    https://github.com/shw7349/1day1git.git
    git commit -m message

---
+++ 1Day1Git/src/test/java/com/Day1Git/Day1Git/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Day1Git.Day1Git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
