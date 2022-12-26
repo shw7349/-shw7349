7d72e16dcd49662feccd957fa7fa8be7b58280ba
shw7349 <shw7349@naver.com>
2022-12-26 22:29:31 +0900

    20221216-1

--- 1Day1Git/src/main/java/com/Day1Git/Day1Git/controller/MemberController.java
+++ 1Day1Git/src/main/java/com/Day1Git/Day1Git/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.Day1Git.Day1Git.repository.MemberRepository;
 import com.Day1Git.Day1Git.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -15,6 +16,11 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
     }
     
 }
---
+++ 1Day1Git/src/main/java/com/Day1Git/Day1Git/controller/MemberForm.java
@@ -0,0 +1,16 @@
+package com.Day1Git.Day1Git.controller;
+
+public class MemberForm {
+    
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+
+
+}
