bfd66c51fcd801955e4433118a9e3e648bb6b85a
shw7349 <shw7349@naver.com>
2022-12-31 21:06:29 +0900

    2022.12.31

--- 1Day1Git/src/main/java/com/Day1Git/Day1Git/Springconfig.java
+++ 1Day1Git/src/main/java/com/Day1Git/Day1Git/Springconfig.java
@@ -1,5 +1,7 @@
 package com.Day1Git.Day1Git;
 
+import com.Day1Git.Day1Git.repository.JdbcMemberRepository;
+import com.Day1Git.Day1Git.repository.JdbcTemplateMemberRepository;
 import com.Day1Git.Day1Git.repository.MemberRepository;
 import com.Day1Git.Day1Git.repository.MemoryMemberRepository;
 import com.Day1Git.Day1Git.service.MemberService;
@@ -9,13 +11,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Springconfig {
 
+    private final DataSource dataSource;
+      public SpringConfig(DataSource dataSource) {
+          this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
     }
 
     @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+      public MemberRepository memberRepository() {
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+      }
+ 
 }
